6f65d9ae64e0b872d1c51c9da09dd96fb9a193df
Danilo Toza Todorovic <151542529+Dantoza@users.noreply.github.com>
2025-07-09 09:19:09 +0200

    Add files via upload
    added the file(duh)

---
+++ HACK.bat
@@ -0,0 +1,604 @@
+@echo off
+@echo.
+@echo Welcome to safe extraction program for stealing passwords
+@echo Start password preparation process.Do you want to proceed?
+@echo.
+@echo NOTE:You may experience some visual bugs, but our defender will get rid of them, preventing any harm to the device
+@echo.
+
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+
+PAUSE
+color a
+tree
+color b
+color c
+color 15
+color c
+color b
+color c
+color b
+color  18
+color 10 
+tree 
+color d
+color e
+color 4
+color 10
+color 12
+color 11
+color 9
+color 10
+color 19
+color 11
+color 10
+color 13
+color 9
+color 11
+color 9
+color a
+tree
+dir
+ipconfig
+ping www.google.com
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo ready to attack passwords and steal data.proceed?
+@echo.
+@echo.
+@echo DANGER:You may experience more of the visual glitches due to the anti-virus that the victim device has
+@echo.
+PAUSE
+color a
+tree
+color b
+color c
+color 15
+color c
+color b
+color c
+color b
+color 18
+color 10 
+tree 
+color d
+color 19
+color 4
+color 10
+color 12
+color 11
+color 9
+color 10 
+color 2 
+color 4
+color 10
+color 13
+color 12 
+color 11
+color 9
+color a
+tree
+dir
+ipconfig
+ping www.google.com
+ping www.edge.com
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo Take all bitcoins from the device.proceed?
+@echo.
+@echo NOTE:this device has a very strong antivirus.proceed anyways?
+@echo.
+@echo.
+PAUSE
+@echo.
+@echo.
+SET /A a = %random%
+SET /A b = %random%
+SET /A c = b+a
+@echo mining bitcoin...
+@ping 192.0.2.2 -n 1 -w 1000 > nul
+@echo.
+@echo %a% bitcoin stolen
+@echo.
+@ping 192.0.2.2 -n 1 -w 1000 > nul
+@echo %c% bitcoin stolen
+@echo.
+@ping 192.0.2.2 -n 1 -w 1000 > nul
+@echo.
+@echo stealing important data...
+@ping 192.0.2.2 -n 1 -w 1000 > nul
+@echo.
+@echo stealing credit card number...
+@ping 192.0.2.2 -n 1 -w 2000 > nul
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@shutdown /s /t 11 /c "EROR_SELF_DESTRUCT_IN_10_SECONDS "
+@ping 192.0.2.2 -n 1 -w 500 > nul
+@echo ___________________________________________________________
+@echo SOMETHING WENT WRONG. SELF DESTUCT IN 10 SECONDS
+@echo ___________________________________________________________
+@ping 192.0.2.2 -n 1 -w 1000 > nul
+@echo 10
+@ping 192.0.2.2 -n 1 -w 1000 > nul
+@echo 9
+@ping 192.0.2.2 -n 1 -w 1000 > nul
+@echo 8
+@ping 192.0.2.2 -n 1 -w 1000 > nul
+@echo 7
+@ping 192.0.2.2 -n 1 -w 1000 > nul
+@echo 6
+@ping 192.0.2.2 -n 1 -w 1000 > nul
+@echo 5
+@ping 192.0.2.2 -n 1 -w 1000 > nul
+@echo 4
+@ping 192.0.2.2 -n 1 -w 1000 > nul
+@echo 3
+@ping 192.0.2.2 -n 1 -w 1000 > nul
+@echo 2
+@ping 192.0.2.2 -n 1 -w 1000 > nul
+@echo 1
+@shutdown/a
+@ping 192.0.2.2 -n 1 -w 200 > nul
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo ________________________________________
+@echo Suscessfuly saved the device!!Continue?
+@echo ________________________________________
+
+@PAUSE
+@echo mining bitcoin...
+@ping 192.0.2.2 -n 1 -w 1000 > nul
+@echo.
+
+SET /A a = %random%
+@echo %a% bitcoin stolen
+@echo.
+SET /A b = %random%
+SET /A c = a+b
+@ping 192.0.2.2 -n 1 -w 1000 > nul
+@echo %c% bitcoin stolen
+@echo.
+@ping 192.0.2.2 -n 1 -w 1000 > nul
+@echo.
+@echo stealing important data...
+@ping 192.0.2.2 -n 1 -w 1000 > nul
+@echo.
+@echo stealing credit card number...
+@echo credit card number:
+@ping 192.0.2.2 -n 1 -w 3000 > nul
+cls
+@echo.
+@ping 192.0.2.2 -n 1 -w 1000 > nul
+@echo 5 _ _ _ _ _ _ _ _ _ _ _ _
+@ping 192.0.2.2 -n 1 -w 3000 > nul
+cls
+
+
+@echo 5 9 _ _ _ _ _ _ _ _ _ _ _ 
+
+@ping 192.0.2.2 -n 1 -w 2000 > nul
+cls
+@echo 5 9 3 _ _ _ _ _ _ _ _ _ _ 
+
+@ping 192.0.2.2 -n 1 -w 1000 > nul
+cls
+@echo 5 9 3 8 _ _ _ _ _ _ _ _ _ 
+
+@ping 192.0.2.2 -n 1 -w 4000 > nul
+cls
+
+@echo 5 9 3 8 6 _ _ _ _ _ _ _ _ 
+
+@ping 192.0.2.2 -n 1 -w 1000 > nul
+cls
+@echo 5 9 3 8 6 7 _ _ _ _ _ _ _ 
+@ping 192.0.2.2 -n 1 -w 1000 > nul
+cls
+@echo 5 9 3 8 6 7 1 _ _ _ _ _ _ 
+@ping 192.0.2.2 -n 1 -w 1000 > nul
+cls
+@echo 5 9 3 8 6 7 1 0 _ _ _ _ _ 
+@ping 192.0.2.2 -n 1 -w 1000 > nul
+cls
+@echo 5 9 3 8 6 7 1 0 2 _ _ _ _ 
+@ping 192.0.2.2 -n 1 -w 1000 > nul
+cls
+@echo 5 9 3 8 6 7 1 0 2 7 _ _ _ 
+@ping 192.0.2.2 -n 1 -w 1000 > nul
+cls
+@echo 5 9 3 8 6 7 1 0 2 7 9 _ _  
+
+@ping 192.0.2.2 -n 1 -w 1000 > nul
+cls
+@echo 5 9 3 8 6 7 1 0 2 7 9 4 _ 
+
+@ping 192.0.2.2 -n 1 -w 1000 > nul
+cls
+@echo 5 9 3 8 6 7 1 0 2 7 9 4 #
+
+
+
+@ping 192.0.2.2 -n 1 -w 3000 > nul
+@echo trying to uncover the last number...
+@ping 192.0.2.2 -n 1 -w 2000 > nul
+@echo.
+@echo please wait...
+@ping 192.0.2.2 -n 1 -w 2000 > nul
+@color b
+@color c
+@color 15
+@color c
+@color b
+@color c
+@color b
+@color 18
+@color 10 
+@color 19
+@color 4
+@color 10
+@color 12
+@color 11
+@color 9
+@color 10 
+@color 2 
+@color 4
+@color 10
+@color 13
+@color 12 
+@color 11
+@color 9
+@color a
+@color b
+@color c
+@color 15
+@color c
+@color b
+@color c
+@color b
+@color 18
+@color 10 
+@color 19
+@color 4
+@color 10
+@color 12
+@color 11
+@color 9
+@color 10 
+@color 2 
+@color 4
+@color 10
+@color 13
+@color 12 
+@color 11
+@color 9
+@color a
+
+@color b
+@color c
+@color 15
+@color c
+@color b
+@color c
+@color b
+@color 18
+@color 10 
+@color 19
+@color 4
+@color 10
+@color 12
+@color 11
+@color 9
+@color 10 
+@color 2 
+@color 4
+@color 10
+@color 13
+@color 12 
+@color 11
+@color 9
+@color a
+
+
+@shutdown /s /t 60 /c "ANTI_VIRUS_ATTACK_SELF_DESTRUCT_IN_10_SECONDS "
+@shutdown/a
+@echo deleted the attack
+@ping 192.0.2.2 -n 1 -w 4000 > nul
+@shutdown /s /t 60 /c "ANTI_VIRUS_ATTACK_SELF_DESTRUCT_IN_10_SECONDS "
+@ping 192.0.2.2 -n 1 -w 4000 > nul
+@shutdown/a
+@echo deleted the attack
+@ping 192.0.2.2 -n 1 -w 4000 > nul
+@shutdown /s /t 60 /c "ANTI_VIRUS_ATTACK_SELF_DESTRUCT_IN_10_SECONDS "
+@ping 192.0.2.2 -n 1 -w 4000 > nul
+@shutdown/a
+@echo deleted the attack
+@ping 192.0.2.2 -n 1 -w 4000 > nul
+
+
+
+
+
+
+
+cls
+@echo TOO DANGEROUS FOR STEALING DATA!!!! YOU ARE BEING LOGGED OUT...
+@ping 192.0.2.2 -n 1 -w 5000 > nul
+@echo changing ip adress...
+@ping 192.0.2.2 -n 1 -w 3000 > nul
+@echo setting up VPN...
+@ping 192.0.2.2 -n 1 -w 3000 > nul
+@echo changing user data...
+@ping 192.0.2.2 -n 1 -w 3000 > nul
+cls
+
+@color 57
+@echo.
+@echo.
+@echo.
+@echo Welcome to safe incognito program for infiltrating into servers
+@echo Start server locating process.Do you want to proceed?
+@echo.
+@echo NOTE:You may experience some visual bugs, but our defender will get rid of them, preventing any harm to the device
+@echo.
+@PAUSE
+color a
+tree
+color b
+color c
+color 15
+color c
+color b
+color c
+color b
+color  18
+color 10 
+tree 
+color d
+color e
+color 4
+color 10
+color 12
+color 11
+color 9
+color 10
+color 19
+color 11
+color 10
+color 13
+color 9
+color 11
+color 9
+color a
+tree
+dir
+ipconfig
+cls
+@echo location found.infiltrate into the server?
+@echo.
+@echo.
+@echo.
+@PAUSE
+@tracert www.google.com
+@echo.
+@echo.
+@echo.
+@echo Finished tracing route to the server
+@echo.
+@ping 192.0.2.2 -n 1 -w 2000 > nul
+@echo reading data...
+@ping 192.0.2.2 -n 1 -w 500 > nul
+@echo this might take a while...
+@ping 192.0.2.2 -n 1 -w 3000 > nul
+@netstat -a
+@ping 192.0.2.2 -n 1 -w 3000 > nul
+@echo reading server ip adress...
+@ping 192.0.2.2 -n 1 -w 3000 > nul
+@route print
+@ping 192.0.2.2 -n 1 -w 5000 > nul
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo stealing connection drivers...
+@ping 192.0.2.2 -n 1 -w 3000 > nul
+@driverquery
+@ping 192.0.2.2 -n 1 -w 3000 > nul
+
+
+mode con: cols=16 lines=16
+
+@echo off
+
+:loop
+
+msg * ERROR
+
+
+goto loop
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo.
+@echo DO not click any key ,keep clicking on ok
+@PAUSE
+
+
+
+
+
+
+
+
+
+
